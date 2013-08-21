b81040a468e456bb3d8959401bb22d74f7aa1385
Joseph Plant <git@JosephPlant.com>
2013-08-21 12:51:36 +1000

    Added jstorage library

--- src/Output-Bootstrap.cmd
+++ src/Output-Bootstrap.cmd
@@ -8,6 +8,7 @@ mkdir Output
 
 xcopy "..\lib\bootstrap 2.3.2" Output\bootstrap /E /Y /I
 xcopy "..\lib\jquery 1.10.2" Output\jquery /E /Y /I
+xcopy "..\lib\jstorage 0.4.4" Output\jstorage /E /Y /I
 
 set nxslt=..\lib\nxslt\nxslt.exe
 
